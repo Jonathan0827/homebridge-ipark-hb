88cbcbeb23e9c9a57fc9d041b83d69c3bc92a18e
Jonathan0827 <limjunehyeop@gmail.com>
2026-05-01 23:26:52 +0900

    patch

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ class Platform {
         while (true) {
             try {
                 const res = await this.http.get(`/light/${j}/apply`);
-                if (res.data.result === "fail") break;
+                if (res.data.result !== "ok") break;
 
                 const roomName = res.data.map?.name || `Room ${j}`;
                 const units = res.data.units || [];
