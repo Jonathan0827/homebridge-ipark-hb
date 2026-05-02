5d195e8299fb9be50a5a50e22eeaed5520e449e4
Jonathan0827 <limjunehyeop@gmail.com>
2026-05-02 23:28:20 +0900

    elevator

---
+++ elevator.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+
+class Elevator {
+    constructor(platform, acc) {
+        this.platform = platform;
+        this.acc = acc;
+
+        const Service = platform.api.hap.Service;
+        const Characteristic = platform.api.hap.Characteristic;
+
+        const service =
+            acc.getService(Service.Thermostat) ||
+            acc.addService(Service.Thermostat);
+
+        service
+            .getCharacteristic(Characteristic.CurrentHeatingCoolingState)
+            .onGet(this.getCurrentState.bind(this))
+            .onSet(this.setTargetState.bind(this));
+    }
+    static discoverDevices(platform) {
+        return [
+            {
+                id: "elevator-up",
+                type: "button",
+                unit: "up",
+                name: "Elevator Up",
+            },
+            {
+                id: "elevator-down",
+                type: "button",
+                unit: "down",
+                name: "Elevator Down",
+            },
+        ];
+    }
+    async getCurrentState() {
+        return false;
+    }
+    async setTargetState(value) {
+        const { platform, acc } = this;
+
+        platform.token = await platform.getAccessToken();
+
+        const res = await platform
+            .http(platform.token)
+            .post(`/elevator/${acc.context.unit}`, acc.context.unit);
+    }
+}
+
+module.exports = Elevator;
